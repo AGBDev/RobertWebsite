0e364dece20f9b2ab6ee82d9d93e62910e3314fe
AGBDev <nils19052006@gmail.com>
2023-09-26 18:55:48 +0200

    gg

--- src/lib/components/navigationbar/NavigationBar.svelte
+++ src/lib/components/navigationbar/NavigationBar.svelte
@@ -22,8 +22,11 @@
         </li>
     </ul> -->
     <NavBarButton text="Startseite" site="/?site="></NavBarButton>
+    |
     <NavBarButton text="Blog" site="/?site=blog"></NavBarButton>
+    |
     <NavBarButton text="Links" site="/?site=links"></NavBarButton>
+    |
     <NavBarButton text="Kontakt" site="/?site="></NavBarButton>
 </div>
 
