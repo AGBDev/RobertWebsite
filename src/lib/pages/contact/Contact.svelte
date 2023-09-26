dd019816ef6c70790092d8d51171f60cd1370fde
AGBDev <nils19052006@gmail.com>
2023-09-26 19:09:23 +0200

    rr

--- src/lib/pages/contact/Contact.svelte
+++ src/lib/pages/contact/Contact.svelte
@@ -1,7 +1,7 @@
 <h1>Kontakt</h1>
 <h2>Buchungen und Fragen.</h2>
 
-<a href = "mailto: robertlennartz@gmail.com" class="button">EMail Senden</a>
+<a href = "mailto: robertlennartz1@gmail.com" class="button">EMail Senden</a>
 
 <style>
     a.button {
