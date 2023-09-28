19fcb56acf8cf18720d6e881098fe04d0072522a
AGBDev <nils19052006@gmail.com>
2023-09-28 17:55:39 +0200

    fixed some html issues

--- src/lib/pages/Home.svelte
+++ src/lib/pages/Home.svelte
@@ -1,6 +1,6 @@
 <div class="gfg">
     <img src=
-                 "robert.jpg">
+                 "robert.jpg" alt="">
     <div class="text-container">
         <h2>DJ Robert Lenz</h2>
         <p>DJ und Producer</p>
