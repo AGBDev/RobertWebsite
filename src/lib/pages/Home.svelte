6e4d6a87df4c63c5d8d770e706068a4987352df3
AGBDev <nils19052006@gmail.com>
2023-09-28 18:13:11 +0200

    updates: changed facebook logo

--- src/lib/pages/Home.svelte
+++ src/lib/pages/Home.svelte
@@ -1,6 +1,5 @@
 <div class="gfg">
-    <img src=
-                 "robert.jpg" alt="">
+    <img src="robert.jpg" alt="">
     <div class="text-container">
         <h2>DJ Robert Lenz</h2>
         <p>DJ und Producer</p>
