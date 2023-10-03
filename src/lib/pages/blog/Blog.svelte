f74e43a4cf95eb98f0a90782b702e416300da508
AGBDev <nils19052006@gmail.com>
2023-10-03 10:14:51 +0200

    update: each blog entry can now be viewed separately by using an id param in the search. (?id=)

--- src/lib/pages/blog/Blog.svelte
+++ src/lib/pages/blog/Blog.svelte
@@ -7,26 +7,59 @@
 
     let entries: BlogEntry[] = [];
 
+    entries.push({name: "Test", content: ["Hallo!", "Das ist ein cooler Eintrag!"], id: 0});
+    entries.push({name: "Test", content: ["Hallo!", "Das ist ein cooler Eintrag!"], id: 1});
+    entries.push({name: "Test", content: ["Hallo!", "Das ist ein cooler Eintrag!"], id: 2});
+
+    entries.reverse();
+
+    let loadAll = false;
+
+    let urlParams = new URLSearchParams(window.location.search);
+    let id = urlParams.get("id");
+
+    if (id === "" || id == null)
+    {
+        loadAll = true;
+    }
 </script>
 
 <div>
     <h1 style="font-family: 'Times New Roman'">Blog</h1>
+    {#if entries.length === 0}
+        <h2 style="font-family: 'Times New Roman', Times, serif">Zurzeit gibt es keine Einträge.</h2>
+    {/if}
     {#each entries as entry}
-        <div>
-            <h1 style="font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;">{entry.name}</h1>
+        {#if (loadAll)}
+
+        <a href="/blog/?id={entry.id}">
             <div>
-                {#each entry.content as content}
-                    <h2 style="color:antiquewhite; font-size: xx-larger">{content}</h2>
-                {/each}
+                <h1 style="font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;">{entry.name}</h1>
+                <div>
+                    {#each entry.content as content}
+                        <h2 style="color:antiquewhite; font-size: xx-larger">{content}</h2>
+                    {/each}
+                </div>
             </div>
-        </div>
+        </a>
         {#if entries.length > 1}
             {#if entry !== entries[entries.length - 1]}
                 <hr>
             {/if}
         {/if}
+        {:else}
+            {#if (entry.id.toString() === id)}
+                <a href="/blog">Zurück.</a>
+                <div>
+                    <h1 style="font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; font-size: 100px">{entry.name}</h1>
+                    <div>
+                        {#each entry.content as content}
+                            <h1 style="color:antiquewhite; font-size: xx-larger">{content}</h1>
+                        {/each}
+                    </div>
+                </div>
+            {/if}
+        {/if}
+
     {/each}
-    {#if entries.length === 0}
-        <h2 style="font-family: 'Times New Roman', Times, serif">Zurzeit gibt es keine Einträge.</h2>
-    {/if}
 </div>
