9fe1d0daf048980fb69854fc6dcc0ea8fc031a01
AGBDev <nils19052006@gmail.com>
2023-10-04 14:21:52 +0200

    changed formatting cause it sucked :C

--- src/lib/pages/links/Links.svelte
+++ src/lib/pages/links/Links.svelte
@@ -4,11 +4,8 @@
     <h2 class="sitesubheader">DJ Robert Lenz ist auch auf den folgenden Platformen zu finden</h2>
 
     <hr>
-
-    <a
-            href="https://soundcloud.com/djrobertlenz"
-            style="font-size: larger;"
-    >
+    
+    <a href="https://soundcloud.com/djrobertlenz" style="font-size: larger;">
         <h3>Soundcloud</h3>
         <div class="gfg">
             <img src="Soundcloud-Logo.png" width="200" alt=""/>
@@ -23,10 +20,7 @@
 
     <hr>
 
-    <a
-            href="https://www.facebook.com/robert.lennartz.923"
-            style="font-size: larger;"
-    >
+    <a href="https://www.facebook.com/robert.lennartz.923" style="font-size: larger;" >
         <h3>Facebook</h3>
         <div class="gfg">
             <img src="facebook.png" width="250" alt=""/>
