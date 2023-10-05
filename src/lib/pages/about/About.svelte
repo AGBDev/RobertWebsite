ef8008b44ad32d7a35eb80af2bcb7807c9b5b736
AGBDev <nils19052006@gmail.com>
2023-10-05 15:41:19 +0200

    improved mobile css and stuff

--- src/lib/pages/about/About.svelte
+++ src/lib/pages/about/About.svelte
@@ -1,9 +1,20 @@
+<script lang="ts">
+    let lines: string[] = [];
+
+    lines.push("Soos");
+    lines.push("Soos");
+</script>
+
+
 <div>
     <h1 class="siteheader">Über mich</h1>
-    <h2 class="sitesubheader">
+    {#each lines as line}
+        <h2 class="sitesubheader">{line}</h2>
+    {/each}
+    <!-- <h2 class="sitesubheader">
         Hallo, meine Name ist Robert Lenz.
     </h2>
     <h2 class="sitesubheader">
         Ich bin DJ und Producer aus dem Raum Aachen.
-    </h2>
+    </h2> -->
 </div>
